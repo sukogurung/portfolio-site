e0625e397a5053b93a6fbc062b0037e7c67b5fad
SukBahadur <sukbahadurg25@outlook.com>
2025-02-18 23:44:06 -0800

    Favicon updated + fixed footer on small devices

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   
 
   return (
-    <div className='w-full flex flex-col items-center justify-center py-2'>
-      <div className='w-full border-t pt-1' style={{ borderColor: 'var(--footer-border-color)' }}></div>
+    <div className='w-full flex flex-col items-center justify-center mb-2'>
+      <div className='w-full border-t' style={{ borderColor: 'var(--footer-border-color)' }}></div>
       <div className='flex flex-row items-center justify-center gap-6'>
         <div className='text-gray-700 text-sm'>&copy; Suko Gurung</div>
         <a
